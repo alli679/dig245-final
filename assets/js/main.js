d7e017810ace576c50bbc8f63580bcdd6a8f4daf
Alexander Li <alli@davidson.edu>
2022-11-16 00:32:21 -0500

    Made the other 7 goals draggable

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,62 @@ $(".goal-1").draggable({
   },
 });
 
+$(".goal-2").draggable({
+  drag: function(){
+    $(".goal-2").css({
+      "z-index": ++dragZ
+    });
+  },
+});
+
+$(".goal-3").draggable({
+  drag: function(){
+    $(".goal-3").css({
+      "z-index": ++dragZ
+    });
+  },
+});
+
+$(".goal-4").draggable({
+  drag: function(){
+    $(".goal-4").css({
+      "z-index": ++dragZ
+    });
+  },
+});
+
+$(".goal-5").draggable({
+  drag: function(){
+    $(".goal-5").css({
+      "z-index": ++dragZ
+    });
+  },
+});
+
+$(".goal-6").draggable({
+  drag: function(){
+    $(".goal-6").css({
+      "z-index": ++dragZ
+    });
+  },
+});
+
+$(".goal-7").draggable({
+  drag: function(){
+    $(".goal-7").css({
+      "z-index": ++dragZ
+    });
+  },
+});
+
+$(".goal-8").draggable({
+  drag: function(){
+    $(".goal-8").css({
+      "z-index": ++dragZ
+    });
+  },
+});
+
 $(".goal-area").droppable({
   drop: function(){
     goalSelected = true;
